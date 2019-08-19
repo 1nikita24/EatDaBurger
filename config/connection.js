15f2d8c6f06b9f7cc17381597a08b3fd7e8d2a1f
1nikita24 <nikitaezhov@icloud.com>
2019-08-18 20:54:00 -0400

    heroku build

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,20 @@
-const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burger_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
+var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password", // add your local password here.
+    database: "burger_db" // add your db name here
+  });
+}
+connection.config.typeCast = function(field, next) {
+  if (field.type == "TINY" && field.length == 1) {
+    return field.string() == "1"; // 1 = true, 0 = false
   }
-  console.log("connected as id " + connection.threadId);
-});
-
+  return next();
+};
 module.exports = connection;
